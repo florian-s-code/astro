cbcbf98018e06af74698854ccf437c0d00318c0f
Florian S <florian-s.github@mailo.com>
2022-05-16 19:02:10 +0200

    WIP: add test

--- packages/astro/test/alias.test.js
+++ packages/astro/test/alias.test.js
@@ -24,7 +24,25 @@ describe('Aliases', () => {
 			await devServer.stop();
 		});
 
-		it.only('can load client components', async () => {
+		it('can load client components', async () => {
+			const html = await fixture.fetch('/').then((res) => res.text());
+			const $ = cheerio.load(html);
+
+			// Should render aliased element
+			expect($('#client').text()).to.equal('test');
+
+			const scripts = $('script').toArray();
+			expect(scripts.length).to.be.greaterThan(0);
+		});
+	});
+
+	describe('static build', () => {
+
+		before(async () => {
+			await fixture.build();
+		});
+
+		it('can apply aliases', async () => {
 			const html = await fixture.fetch('/').then((res) => res.text());
 			const $ = cheerio.load(html);
 
